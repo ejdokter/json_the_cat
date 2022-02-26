415b6bbc02b93cee1fb87099db41157033c6fe72
Eric Dokter <edokter11@gmail.com>
2022-02-26 16:10:40 -0700

    complete project

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,35 @@
 const request = require('request');
 
-const breedFetcher = function() {
-  const breed = process.argv.slice(2).join();
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+// const breedFetcher = function() {
+//   const breed = process.argv.slice(2).join();
+//   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       let data = JSON.parse(body);
+//       if (data.length === 0) {
+//         console.log(`${breed} not found.`);
+//       } else {
+//         console.log(data[0].description);
+//       }
+//     }
+//   });
+// };
+
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {  
     if (error) {
-      console.log(error);
+      callback(error, null)
     } else {
-      let data = JSON.parse(body);
-      if (data.length === 0) {
-        console.log(`${breed} not found.`);
-      } else {
-        console.log(data[0].description);
-      }
+        if (body === '[]') {
+          callback(`${breedName} not found`, null)
+        } else {
+          const data = JSON.parse(body)
+          callback(null, data[0].description)
+        }   
     }
   });
 };
 
-breedFetcher();
+
+module.exports = { fetchBreedDescription }
